22a8303fface8e9e532bbedfe9765cff71df1268
Воробьев Михаил <inversionspaces@vivaldi.net>
2021-04-11 10:44:30 +0300

    messy script almost done

--- gen_card.py
+++ gen_card.py
@@ -1,5 +1,7 @@
 import requests
 
+result = ""
+
 resp = requests.get("https://api.scryfall.com/cards/random")
 card = resp.json()
 
@@ -19,24 +21,27 @@ def prepare(string):
 
 
 name = prepare(card["name"])
-oracle_text = card.get("oracle_text") or ""
+oracle_text = prepare(card.get("oracle_text") or "")
 
 type_line = card["type_line"]
 super_type, types, subtypes = parse_type_line(type_line)
 
 if (super_type):
-    print(  f"INSERT INTO mtg.cards (name, oracle_text, super_type_fk) \n"
-            f"VALUES(E'{name}', '{oracle_text}', \n"
-            f"(SELECT id FROM mtg.super_types WHERE name = '{super_type}')); \n" )
+    result += (f"INSERT INTO mtg.cards (name, oracle_text, super_type_fk) \n"
+            f"VALUES(E'{name}', E'{oracle_text}', \n"
+            f"(SELECT id FROM mtg.super_types WHERE name = '{super_type}')); \n\n" )
 else:
-    print(  f"INSERT INTO mtg.cards (name, oracle_text) \n"
-            f"VALUES(E'{name}', '{oracle_text}'); \n" )
+    result += (  f"INSERT INTO mtg.cards (name, oracle_text) \n"
+            f"VALUES(E'{name}', E'{oracle_text}'); \n\n" )
+
+if "Token" in types:
+    exit(0)
 
 for card_type in types:
     if card_type == "Creature":
         power = card["power"]
         toughness = card["toughness"]
-        print(  f"WITH temp(creature_id) AS (\n"
+        result += (  f"WITH returns(id) AS (\n"
                 f"INSERT INTO mtg.creatures (power, toughness) \n"
                 f"VALUES ({power}, {toughness}) \n"
                 f"RETURNING id\n"
@@ -45,10 +50,10 @@ for card_type in types:
                 f"VALUES( \n"
                 f"(SELECT id FROM mtg.cards WHERE name = E'{name}'), \n"
                 f"(SELECT id FROM mtg.types WHERE name = '{card_type}'), \n"
-                f"temp.creature_id); \n" )
+                f"(SELECT id FROM returns)); \n\n" )
     elif card_type == "Planeswalker":
         loyalty = card["loyalty"]
-        print(  f"WITH temp(planeswalker_id) AS (\n"
+        result += (  f"WITH returns(id) AS (\n"
                 f"INSERT INTO mtg.planeswalkers (starting_loyalty) \n"
                 f"VALUES ({loyalty}) \n"
                 f"RETURNING id\n"
@@ -57,24 +62,21 @@ for card_type in types:
                 f"VALUES( \n"
                 f"(SELECT id FROM mtg.cards WHERE name = E'{name}'), \n"
                 f"(SELECT id FROM mtg.types WHERE name = '{card_type}'), \n"
-                f"temp.planeswalker_id); \n" )
+                f"(SELECT id FROM returns)); \n\n" )
     else:
-        print(  f"INSERT INTO mtg.card_types (card_fk, type_fk) \n"
+        result += (  f"INSERT INTO mtg.card_types (card_fk, type_fk) \n"
                 f"VALUES( \n"
                 f"(SELECT id FROM mtg.cards WHERE name = E'{name}'), \n"
-                f"(SELECT id FROM mtg.types WHERE name = '{card_type}')); \n" )
+                f"(SELECT id FROM mtg.types WHERE name = '{card_type}')); \n\n" )
 
 for sub_type in subtypes:
-    print(  f"WITH temp(sub_type_id) AS (\n"
-            f"INSERT INTO mtg.sub_types (name) \n"
+    result += (  f"INSERT INTO mtg.sub_types (name) \n"
             f"VALUES ('{sub_type}') \n"
-            f"ON CONFLICT DO NOTHING \n"
-            f"RETURNING id\n"
-            f") \n"
+            f"ON CONFLICT DO NOTHING; \n\n"
             f"INSERT INTO mtg.card_subtypes (card_fk, sub_type_fk) \n"
             f"VALUES( \n"
             f"(SELECT id FROM mtg.cards WHERE name = E'{name}'), \n"
-            f"temp.sub_type_id); \n" )
+            f"(SELECT id FROM mtg.sub_types WHERE name = '{sub_type}')); \n\n" )
 
 mana_cost = card["mana_cost"]
 
@@ -82,22 +84,25 @@ import re
 from collections import Counter
 
 def parse_mana_cost(mana_cost):
-    r = re.compile("{(\d*|[WUBGRX])}")
     if (not mana_cost):
         return []
+    r = re.compile("{(\d*|[WUBGRX])}")
     parsed = r.findall(mana_cost)
-    first = parsed[0]
-    if (first.isdigit()):
-        return [('A', int(first))] + list(Counter(parsed[1:]).items())
+    symbols = [s for s in parsed if s.isalpha()]
+    numbers = [int(s) for s in parsed if s.isdigit()]
+    counted = list(Counter(symbols).items())
+    if not numbers:
+        return counted
     else:
-        return list(Counter(parsed).items())
+        return counted + [('A', sum(numbers))]
+
 
 for (abbr, cnt) in parse_mana_cost(mana_cost):
-    print(  f"INSERT INTO mtg.mana_cost (card_fk, mana_type_fk, amount) \n"
+    result += (  f"INSERT INTO mtg.mana_cost (card_fk, mana_type_fk, amount) \n"
             f"VALUES( \n"
             f"(SELECT id FROM mtg.cards WHERE name = E'{name}'), \n"
             f"(SELECT id FROM mtg.mana_types WHERE abbr = '{abbr}'), \n"
-            f"{cnt}); \n")
+            f"{cnt}); \n\n")
 
 resp = requests.get(card["prints_search_uri"])
 prints = resp.json()["data"]
@@ -111,10 +116,12 @@ for print_ in sample(prints, min(randint(1, 5), len(prints))):
     set_code = print_["set"]
     image_url = print_["image_uris"]["normal"]
 
-    print(  f"INSERT INTO mtg.representations (image_url, image_artist, flavor_text, \n"
+    result += (  f"INSERT INTO mtg.representations (image_url, image_artist, flavor_text, \n"
             f"card_id, set_id, rarity) \n"
             f"VALUES('{image_url}', '{artist}', E'{flavor_text}', \n"
             f"(SELECT id FROM mtg.cards WHERE name = E'{name}'), \n"
             f"(SELECT id FROM mtg.sets WHERE code = '{set_code}'), \n"
-            f"'{rarity}'); \n" )
+            f"'{rarity}'); \n\n" )
 
+result += "\n" + "-" * 60 + "\n"
+print(result)
